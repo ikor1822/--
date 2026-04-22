682e0a358da1867f7ddcb6dae194d5bc53c0cf29
Ivan <ikor12945@gmail.com>
2026-04-22 18:18:53 +0300

    Add files via upload

--- LP/GoFun.fsx
+++ LP/GoFun.fsx
@@ -1,15 +1,8 @@
 open System
 open System.IO
-open System.Text.RegularExpressions
 
-// ==========================================
-// 1. АБСТРАКТНОЕ СИНТАКСИЧЕСКОЕ ДЕРЕВО (AST)
-// ==========================================
 type Expr =
-    | Num of int
-    | Str of string
-    | Bool of bool
-    | Var of string
+    | Num of int | Str of string | Bool of bool | Var of string
     | List of Expr list
     | Assign of string * Expr
     | Func of string * string list * Expr
@@ -19,227 +12,221 @@ type Expr =
     | Block of Expr list
 
 type Value =
-    | VNum of int
-    | VStr of string
-    | VBool of bool
+    | VNum of int | VStr of string | VBool of bool
     | VList of Value list
     | VClosure of string * string list * Expr * ref<Map<string, Value>>
     | VNative of (Value list -> Value)
 
-and Environment = Map<string, Value>
+type Env = Map<string, Value>
 
-// ==========================================
-// 2. ЛЕКСЕР
-// ==========================================
-let tokenize (code: string) =
-    let pattern = @"\""[^\n]*?\""|[a-zA-Z_][a-zA-Z0-9_]*|\d+|==|<=|>=|<|>|[{}()=+\-*/,\[\]]"
-    [ for m in Regex.Matches(code, pattern) -> m.Value ]
-
-// ==========================================
-// 3. РУЧНОЙ ПАРСЕР
-// ==========================================
-let rec parseExpr toks =
-    let e1, t1 = parseTerm toks
-    let rec loop left ts =
-        match ts with
-        | op :: rest when op = "+" || op = "-" || op = "*" || op = "==" || op = "<" || op = ">" ->
-            let right, nextTs = parseTerm rest
-            loop (BinOp(op, left, right)) nextTs
-        | _ -> left, ts
-    loop e1 t1
-
-and parseTerm toks =
-    match toks with
-    | "if" :: t1 ->
-        let cond, t2 = parseExpr t1
-        let trueBr, t3 = parseBlock t2
-        match t3 with
-        | "else" :: t4 ->
-            let falseBr, t5 = parseBlock t4
-            If(cond, trueBr, falseBr), t5
-        | _ -> If(cond, trueBr, Block []), t3
-        
-    | "func" :: name :: "(" :: t1 ->
-        let rec parseParams ts acc =
-            match ts with
-            | ")" :: rest -> List.rev acc, rest
-            | "," :: rest -> parseParams rest acc
-            | p :: rest -> parseParams rest (p :: acc)
-            // ИСПРАВЛЕНИЕ: Добавлена обработка внезапного конца файла в аргументах
-            | [] -> failwith "Ожидалась закрывающая скобка ')' в аргументах функции"
-            
-        let args, t2 = parseParams t1 []
-        let body, t3 = parseBlock t2
-        Func(name, args, body), t3
-        
-    | name :: "=" :: t1 ->
-        let expr, t2 = parseExpr t1
-        Assign(name, expr), t2
-        
-    | name :: "(" :: t1 ->
-        let rec parseArgs ts acc =
-            match ts with
-            | ")" :: rest -> List.rev acc, rest
-            | "," :: rest -> parseArgs rest acc
-            | _ ->
-                let e, rest = parseExpr ts
-                parseArgs rest (e :: acc)
-        let args, t2 = parseArgs t1 []
-        Call(name, args), t2
-        
-    | "[" :: t1 ->
-        let rec parseList ts acc =
-            match ts with
-            | "]" :: rest -> List (List.rev acc), rest
-            | "," :: rest -> parseList rest acc
-            | _ ->
-                let e, rest = parseExpr ts
-                parseList rest (e :: acc)
-        parseList t1 []
-        
-    | "(" :: t1 ->
-        let e, t2 = parseExpr t1
-        match t2 with
-        | ")" :: t3 -> e, t3
-        | _ -> failwith "Ожидалась закрывающая скобка ')'"
-        
-    | "{" :: _ as t1 -> 
-        parseBlock t1
-        
-    | "true" :: t1 -> Bool true, t1
-    | "false" :: t1 -> Bool false, t1
-    | str :: t1 when str.StartsWith("\"") -> Str (str.Trim('"')), t1
-    | num :: t1 when Char.IsDigit(num.[0]) -> Num (int num), t1
-    | var :: t1 when Char.IsLetter(var.[0]) || var.[0] = '_' -> Var var, t1
-    | [] -> failwith "Неожиданный конец файла"
-    | tok :: _ -> failwithf "Синтаксическая ошибка: %s" tok
-
-and parseBlock toks =
-    match toks with
-    | "{" :: t1 ->
-        let rec parseStmts ts acc =
-            match ts with
-            | "}" :: rest -> Block (List.rev acc), rest
-            | _ ->
-                let e, rest = parseExpr ts
-                parseStmts rest (e :: acc)
-        parseStmts t1 []
-    | _ -> failwith "Ожидалось начало блока '{'"
-
-let parseProgram toks =
-    let rec loop ts acc =
-        if List.isEmpty ts then Block (List.rev acc)
+let tok (s:string) =
+    let rec f i a =
+        if i>=s.Length then List.rev a else
+        let c=s.[i]
+        if Char.IsWhiteSpace c then f (i+1) a else
+        if Char.IsDigit c then
+            let mutable j=i
+            while j<s.Length && Char.IsDigit s.[j] do j<-j+1
+            f j (s.Substring(i,j-i)::a)
+        elif Char.IsLetter c || c='_' then
+            let mutable j=i
+            while j<s.Length && (Char.IsLetter s.[j] || Char.IsDigit s.[j] || s.[j]='_') do j<-j+1
+            f j (s.Substring(i,j-i)::a)
+        elif c='"' then
+            let mutable j=i+1
+            while j<s.Length && s.[j]<>'"' do j<-j+1
+            f (j+1) (s.Substring(i,j-i+1)::a)
         else
-            let e, rest = parseExpr ts
-            loop rest (e :: acc)
-    loop toks []
+            if i+1<s.Length && s.[i]='=' && s.[i+1]='=' then
+                f (i+2) ("=="::a)
+            else
+                f (i+1) ((string c)::a)
+    f 0 []
 
-// ==========================================
-// 4. ВЫЧИСЛИТЕЛЬ (EVALUATOR)
-// ==========================================
-let rec eval (env: Environment) (expr: Expr) : Value * Environment =
-    match expr with
-    | Num n -> VNum n, env
-    | Str s -> VStr s, env
-    | Bool b -> VBool b, env
-    | Var x -> Map.find x env, env
-    
-    | List exprs ->
-        let vals = List.map (fun e -> fst (eval env e)) exprs
-        VList vals, env
-    
-    | Assign (name, e) ->
-        let v, _ = eval env e
-        v, Map.add name v env
-        
-    | Func (name, args, body) ->
-        let envRef = ref env
-        let closure = VClosure(name, args, body, envRef)
-        let newEnv = Map.add name closure env
-        envRef.Value <- newEnv
-        closure, newEnv
-        
-    | Call (fName, argsExpr) ->
-        let argsValues = List.map (fun e -> fst (eval env e)) argsExpr
-        match Map.find fName env with
-        | VNative f -> f argsValues, env
-        | VClosure (_, argNames, body, cEnvRef) ->
-            let callEnv = List.fold2 (fun acc name v -> Map.add name v acc) cEnvRef.Value argNames argsValues
-            let res, _ = eval callEnv body
-            res, env
-        | _ -> failwith "Это не функция!"
-        
-    | If (cond, tBranch, fBranch) ->
-        let cVal, _ = eval env cond
-        match cVal with
-        | VBool true -> eval env tBranch
-        | VBool false -> eval env fBranch
-        | _ -> failwith "Условие в if должно быть булевым"
-        
-    | BinOp (op, e1, e2) ->
-        let v1, _ = eval env e1
-        let v2, _ = eval env e2
-        match op, v1, v2 with
-        | "+", VNum a, VNum b -> VNum (a + b), env
-        | "-", VNum a, VNum b -> VNum (a - b), env
-        | "*", VNum a, VNum b -> VNum (a * b), env
-        | "==", VNum a, VNum b -> VBool (a = b), env
-        | "<", VNum a, VNum b -> VBool (a < b), env
-        | ">", VNum a, VNum b -> VBool (a > b), env
-        | _ -> failwith "Ошибка операции"
-        
-    | Block exprs ->
-        let rec evalStmts es currentEnv lastVal =
-            match es with
-            | [] -> lastVal, currentEnv
-            | e :: rest ->
-                let v, nextEnv = eval currentEnv e
-                evalStmts rest nextEnv v
-        evalStmts exprs env (VNum 0)
+let rec pe t =
+    let a,b = pt t
+    let rec g l ts =
+        match ts with
+        | op::r when op="+"||op="-"||op="*"||op="=="||op="<"||op=">" ->
+            let x,y = pt r
+            g (BinOp(op,l,x)) y
+        | _ -> l,ts
+    g a b
 
-// ==========================================
-// 5. СТАНДАРТНАЯ БИБЛИОТЕКА
-// ==========================================
-let rec formatVal v =
+and pt t =
+    match t with
+    | "if"::r ->
+        let c,t1 = pe r
+        let t2,t3 = pb t1
+        match t3 with
+        | "else"::t4 ->
+            let f1,t5 = pb t4
+            If(c,t2,f1),t5
+        | _ -> If(c,t2,Block []),t3
+
+    | "func"::n::"("::r ->
+        let rec h ts a =
+            match ts with
+            | ")"::x -> List.rev a,x
+            | ","::x -> h x a
+            | x::xs -> h xs (x::a)
+            | _ -> failwith "func args"
+        let args,t1 = h r []
+        let b,t2 = pb t1
+        Func(n,args,b),t2
+
+    | n::"="::r ->
+        let e,t2 = pe r
+        Assign(n,e),t2
+
+    | n::"("::r ->
+        let rec h ts a =
+            match ts with
+            | ")"::x -> List.rev a,x
+            | ","::x -> h x a
+            | _ ->
+                let e,x = pe ts
+                h x (e::a)
+        let args,t1 = h r []
+        Call(n,args),t1
+
+    | "["::r ->
+        let rec h ts a =
+            match ts with
+            | "]"::x -> List(List.rev a),x
+            | ","::x -> h x a
+            | _ ->
+                let e,x = pe ts
+                h x (e::a)
+        h r []
+
+    | "("::r ->
+        let e,t2 = pe r
+        match t2 with
+        | ")"::x -> e,x
+        | _ -> failwith ")"
+
+    | "{"::_ -> pb t
+
+    | "true"::r -> Bool true,r
+    | "false"::r -> Bool false,r
+    | x::r when x.StartsWith("\"") -> Str(x.Trim('"')),r
+    | x::r when Char.IsDigit x.[0] -> Num(int x),r
+    | x::r -> Var x,r
+    | _ -> failwith "parse"
+
+and pb t =
+    match t with
+    | "{"::r ->
+        let rec h ts a =
+            match ts with
+            | "}"::x -> Block(List.rev a),x
+            | _ ->
+                let e,x = pe ts
+                h x (e::a)
+        h r []
+    | _ -> failwith "{"
+
+let prog t =
+    let rec f ts a =
+        match ts with
+        | [] -> Block(List.rev a)
+        | _ ->
+            let e,r = pe ts
+            f r (e::a)
+    f t []
+
+let rec ev env e =
+    match e with
+    | Num n -> VNum n,env
+    | Str s -> VStr s,env
+    | Bool b -> VBool b,env
+
+    | Var x ->
+        match Map.tryFind x env with
+        | Some v -> v,env
+        | None -> failwith ("нет переменной: " + x)
+
+    | List xs ->
+        let vs = List.map (fun x -> fst (ev env x)) xs
+        VList vs,env
+
+    | Assign(n,e) ->
+        let v,_ = ev env e
+        v,Map.add n v env
+
+    | Func(n,a,b) ->
+        let r = ref env
+        let f = VClosure(n,a,b,r)
+        let e2 = Map.add n f env
+        r.Value <- e2
+        f,e2
+
+    | Call(n,a) ->
+        let vs = List.map (fun x -> fst (ev env x)) a
+        match Map.tryFind n env with
+        | Some (VNative f) -> f vs,env
+        | Some (VClosure(_,names,b,r)) ->
+            let e2 = List.fold2 (fun acc k v -> Map.add k v acc) r.Value names vs
+            let v,_ = ev e2 b
+            v,env
+        | _ -> failwith ("нет функции: " + n)
+
+    | If(c,t,f) ->
+        let v,_ = ev env c
+        match v with
+        | VBool true -> ev env t
+        | VBool false -> ev env f
+        | _ -> failwith "if"
+
+    | BinOp(op,a,b) ->
+        let v1,_ = ev env a
+        let v2,_ = ev env b
+        match op,v1,v2 with
+        | "+",VNum x,VNum y -> VNum(x+y),env
+        | "-",VNum x,VNum y -> VNum(x-y),env
+        | "*",VNum x,VNum y -> VNum(x*y),env
+        | "==",VNum x,VNum y -> VBool(x=y),env
+        | "<",VNum x,VNum y -> VBool(x<y),env
+        | ">",VNum x,VNum y -> VBool(x>y),env
+        | _ -> failwith "op"
+
+    | Block xs ->
+        let rec f xs env last =
+            match xs with
+            | [] -> last,env
+            | x::r ->
+                let v,e2 = ev env x
+                f r e2 v
+        f xs env (VNum 0)
+
+let rec s v =
     match v with
-    | VStr s -> s
     | VNum n -> string n
+    | VStr x -> x
     | VBool b -> if b then "true" else "false"
-    | VList lst -> "[" + String.Join(", ", List.map formatVal lst) + "]"
-    | _ -> "<функция>"
+    | VList l -> "[" + String.Join(", ", List.map s l) + "]"
+    | _ -> "<f>"
 
-let globalEnv : Environment = 
+let env0 =
     Map.ofList [
-        ("print", VNative (fun args -> 
-            printfn "%s" (formatVal args.[0])
-            VNum 0
-        ));
-        ("head", VNative (fun args -> 
-            match args.[0] with | VList (h::_) -> h | _ -> failwith "Пустой список в head"
-        ));
-        ("tail", VNative (fun args -> 
-            match args.[0] with | VList (_::t) -> VList t | _ -> failwith "Ошибка tail"
-        ));
-        ("cons", VNative (fun args -> 
-            match args.[1] with | VList t -> VList (args.[0] :: t) | _ -> failwith "cons ждет список"
-        ));
-        ("isEmpty", VNative (fun args -> 
-            match args.[0] with | VList [] -> VBool true | VList _ -> VBool false | _ -> failwith "Не список"
-        ))
+        ("print", VNative(fun a -> printfn "%s" (s a.[0]); VNum 0))
+        ("head", VNative(fun a -> match a.[0] with | VList(h::_) -> h | _ -> failwith "h"))
+        ("tail", VNative(fun a -> match a.[0] with | VList(_::t) -> VList t | _ -> failwith "t"))
+        ("cons", VNative(fun a -> match a.[1] with | VList t -> VList(a.[0]::t) | _ -> failwith "c"))
+        ("isEmpty", VNative(fun a -> match a.[0] with | VList [] -> VBool true | _ -> VBool false))
+        ("readFile", VNative(fun a ->
+            match a.[0] with
+            | VStr path -> VStr(File.ReadAllText path)
+            | _ -> failwith "rf"))
     ]
 
-// ==========================================
-// ЗАПУСК
-// ==========================================
-let args = fsi.CommandLineArgs
-if args.Length < 2 then
-    printfn "Использование: dotnet fsi GoFun.fsx <файл.gof>"
+let a = fsi.CommandLineArgs
+
+if a.Length < 2 then
+    printfn "file?"
 else
-    let path = args.[1]
-    if File.Exists path then
-        try
-            let code = File.ReadAllText path
-            let ast = parseProgram (tokenize code)
-            eval globalEnv ast |> ignore
-        with ex -> printfn "ОШИБКА: %s" ex.Message
-    else printfn "Файл не найден!"
+    let code = File.ReadAllText a.[1]
+    let t = tok code
+    let ast = prog t
+    ev env0 ast |> ignore
